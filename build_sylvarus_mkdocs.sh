e502a4a4c2c0ebba5cef8a3a2059581f24f6c913
Silver Swedlund <nathan.swedlund@gmail.com>
2025-03-25 09:27:05 -0400

    update all images to .jpg and add command in build script to auto convert non-jpg images every build

--- build_sylvarus_mkdocs.sh
+++ build_sylvarus_mkdocs.sh
@@ -9,10 +9,12 @@ echo "🔧 Starting Sylvarus MkDocs Build..."
 # List of scripts to run in order
 SCRIPTS=(
   "_scripts/generate_indexes_from_json.py"
+  "_scripts/generate_main_gods_main.py"
+  "_scripts/generate_main_pantheon_index.py"
   "_scripts/generate_pantheon_indexes.py"
-  "_scripts/generate_gods_main_index.py"
 )
 
+# Run Scripts
 for SCRIPT in "${SCRIPTS[@]}"; do
   echo "🚀 Running $SCRIPT..."
   python3 "$SCRIPT"
@@ -21,3 +23,11 @@ for SCRIPT in "${SCRIPTS[@]}"; do
 done
 
 echo "🎉 Sylvarus MkDocs Build Complete!"
+
+# Convert non-jpg images into jpg and trash the non-jpg
+find . -type f \( -iname "*.png" -o -iname "*.jpeg" -o -iname "*.webp" \) -exec sh -c '
+  for img; do
+    new="${img%.*}.jpg"
+    magick "$img" "$new" && trash "$img"
+  done
+' sh {} +
--- docs/pantheons/index.md
+++ docs/pantheons/index.md
@@ -1,1 +1,16 @@
 # Pantheons
+
+Pantheons are coalitions of gods, akin to political parties. These divine groups are self-enforcing, governed internally but adhering loosely to the overarching decisions made by Olympus.
+
+## List of Pantheons
+
+| Pantheon |
+|----------|
+| [Athenians](athenians/index.md) |
+| [Demerans](demerans/index.md) |
+| [Fortunians](fortunians/index.md) |
+| [Gilgamites](gilgamites/index.md) |
+| [Midasians](midasians/index.md) |
+| [None](none/index.md) |
+| [Underworlders](underworlders/index.md) |
+| [Zagarans](zagarans/index.md) |
